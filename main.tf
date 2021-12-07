2cce86a0bd87a1431cdfad3c944bb0e35779af5e
Koushik Kothagal <koush.s@gmail.com>
2021-12-06 16:56:54 -0800

    4.variables 1

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {  
+    required_providers {    
+        aws = {      
+            source  = "hashicorp/aws"      
+            version = "~> 3.27"    
+        }  
+    }
+  required_version = ">= 0.14.9"
+}
+
+variable "app_tag_name" {
+  description = Value of Name tag"
+  type        = string  
+  default     = "app"
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
@@ -7,11 +23,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   tags = {
-    Name = "app"
+    Name = var.app_tag_name
   }
 }
 
 resource "aws_vpc" "app_vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = var.app_tag_name
+  }
 }
 
